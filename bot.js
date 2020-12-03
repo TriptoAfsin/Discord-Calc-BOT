3ab5f3e5da518515f12a7299e48eaf747ee60cb6
TriptoAfsin <afsintripto@gmail.com>
2020-12-03 19:25:45 +0600

    added rock-papepr-scissor

--- bot.js
+++ bot.js
@@ -6,6 +6,7 @@ const Discord = require('discord.js'); //npm install discord.js
 const client = new Discord.Client();
 
 const toss = ["Head", "Tail"];
+const game1 = ["Rock", "Paper", "Scissor"];
 
 //function to check login
 client.on('ready', () => {
@@ -27,7 +28,39 @@ client.on('message', msg => {
         //msg.reply(Math.floor(Math.random()*100));
         msg.channel.send(toss[Math.floor(Math.random()*2)]);
       }
-      
+      else if (msg.content === '!rock' || msg.content === '!paper' || msg.content === '!scissor') {
+        //msg.reply(Math.floor(Math.random()*100));
+        const cpu = game1[Math.floor(Math.random()*3)];
+        if(msg.content === '!rock' && cpu === 'Scissor'){
+          msg.channel.send(cpu);
+          msg.channel.send("You win 💯");
+        }
+        else if(msg.content === '!paper' && cpu === 'Rock'){
+          msg.channel.send(cpu);
+          msg.channel.send("You win 💯");
+        }
+        else if(msg.content === '!scissor' && cpu === 'Paper'){
+          msg.channel.send(cpu);
+          msg.channel.send("You win 💯");
+        }
+        else if(msg.content === '!paper' && cpu === 'Paper'){
+          msg.channel.send(cpu);
+          msg.channel.send("Draw 😀");
+        }
+        else if(msg.content === '!scissor' && cpu === 'Scissor'){
+          msg.channel.send(cpu);
+          msg.channel.send("Draw 😀");
+        }
+        else if(msg.content === '!rock' && cpu === 'Rock'){
+          msg.channel.send(cpu);
+          msg.channel.send("Draw 😀");
+        }
+        else{
+          msg.channel.send(cpu);
+          msg.channel.send("I win 😁");
+        }
+        
+      }
     
 });
   
